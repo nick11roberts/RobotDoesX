af61a410a25b6cbe9dbedb390574d94a9e100592
nick11roberts <n3robert@ucsd.edu>
2016-12-17 21:32:52 -0800

    Changed script to use the desired experiment.

---
+++ experiment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#python single_lstm_rmsprop.py > single_lstm_rmsprop.log
+#python single_gru_rmsprop.py > single_gru_rmsprop.log
+
+python single_lstm_nadam.py > single_lstm_nadam.log
+#python single_gru_nadam.py > single_gru_nadam.log
--- experiment_gruVsLstm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python single_lstm_rmsprop.py > single_lstm_rmsprop.log
-python single_gru_rmsprop.py > single_gru_rmsprop.log
-
-python single_lstm_nadam.py > single_lstm_nadam.log
-python single_gru_nadam.py > single_gru_nadam.log
