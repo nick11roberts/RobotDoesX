853d60f1a5bbfe21b569f882c441806b3ceced39
nick11roberts <n3robert@ucsd.edu>
2016-12-17 23:10:39 -0800

    Fixed create db script

--- create_db.py
+++ create_db.py
@@ -3,4 +3,4 @@ import rethinkdb as r
 r.connect("localhost", 28015).repl()
 
 r.db_create("robot_does_x").run()
-r.db("robot_does_x").create("train_output").run()
+r.db("robot_does_x").table_create("train_output").run()
